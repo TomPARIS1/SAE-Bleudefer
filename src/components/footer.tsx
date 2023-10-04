45f89e4b30a7c1de57de810228d01d58f4427bb1
Zaaaaaat <99033119+Zatosee@users.noreply.github.com>
2023-10-04 11:52:03 +0200

    fix footer
    Footer finish

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,6 +4,10 @@ import './footer.css'
 import Image from 'next/image'
 import instagram from '../img/instagram.png'
 import facebook from '../img/facebook.png'
+import visa from '../img/visa.png'
+import mastercard from '../img/mastercard.png'
+import paypal from '../img/paypal.png'
+import applepay from '../img/apple.png'
 
 export const Footer = function () {
 
@@ -13,17 +17,31 @@ export const Footer = function () {
 
     return (
         <div>
-            <div className="FooterInstagram flex items-center justify-center mt-8">
+            <div className="footerInstagram flex items-center justify-center mt-8">
                 <h1 className="text-xl"> Je suis aussi disponible sur instagram! </h1>
                 <a href={"https://www.instagram.com/bleu_de_fer/"}> https://www.instagram.com/bleu_de_fer/ </a>
             </div>
 
             <hr/>
 
-            <Image src={instagram} width={25} height={25} alt="Logo panier" className="mr-2"/>
+            <div className="containerr px-12 py-12">
+                <div className="under-container">
+                    <div className="reseau">
+                        <Image src={instagram} width={25} height={25} alt="Logo Instagram" className="mr-2" />
+                        <Image src={facebook} width={25} height={25} alt="Logo Facebook" className="mr-2" />
+                    </div>
 
-            <Image src={facebook} width={25} height={25} alt="Logo panier" className="mr-2"/>
-
+                    <div className="paiement">
+                        <Image src={visa} width={25} height={25} alt="Logo Visa" className="mr-2" />
+                        <Image src={mastercard} width={25} height={25} alt="Logo Mastercard" className="mr-2" />
+                        <Image src={paypal} width={25} height={25} alt="Logo PayPal" className="mr-2" />
+                        <Image src={applepay} width={25} height={25} alt="Logo Apple Pay" className="mr-2" />
+                    </div>
+                </div>
+                <div className="footerInstagram flex justify-center mt-8">
+                    <h1 className="copyright"> © 2023, Bleu de fer propulsée par Steve, Tom & l’autre </h1>
+                </div>
+            </div>
         </div>
     );
 }
