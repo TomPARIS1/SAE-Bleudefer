5862f1f843e1431ce96c69c44dbc7e3e5795d679
Zaaaaaat <99033119+Zatosee@users.noreply.github.com>
2023-10-24 16:47:51 +0200

    Create about.tsx

---
+++ src/pages/about.tsx
@@ -0,0 +1,25 @@
+import {Connexion} from "@/components/connexion.tsx";
+import menuC from "@/img/menuC.jpg";
+import '../app/globals.css'
+import TrendingSlider from "@/components/TrendingSlider";
+import Image from 'next/image';
+import {Product} from "@/components/product";
+import bande from "@/img/bande.jpg";
+import {About} from "@/components/about";
+import {Navbar} from "@/components/header";
+import {Footer} from "@/components/footer";
+
+export default function connexion() {
+    return (
+        <main>
+
+            <header>
+                <Navbar/>
+            </header>
+
+            <About/>
+
+            <Footer/>
+        </main>
+    )
+}
