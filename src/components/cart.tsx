35b71b3accbd68b76cbacf1ddc877d8ed3339e82
Steve <112937622+Zaaaaat@users.noreply.github.com>
2023-10-25 08:11:40 +0200

    Update cart.tsx

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -7,13 +7,13 @@ export const Cart = function () {
             <div className="flex shadow-md my-10">
                 <div className="w-3/4 bg-white px-10 py-10">
                     <div className="flex justify-between border-b pb-8">
-                        <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-                        <h2 className="font-semibold text-2xl">3 Items</h2>
+                        <h1 className="font-semibold text-2xl">Panier</h1>
+                        <h2 className="font-semibold text-2xl">6 articles</h2>
                     </div>
                     <div className="flex mt-10 mb-5">
-                        <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
-                        <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Quantity</h3>
-                        <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Price</h3>
+                        <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Détails des produits</h3>
+                        <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Quantité(s)</h3>
+                        <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Prix</h3>
                         <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
                     </div>
                     <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
@@ -53,33 +53,34 @@ export const Cart = function () {
                             <path
                                 d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/>
                         </svg>
-                        Continue Shopping
+                        Retourner à la boutique
                     </a>
                 </div>
 
                 <div id="summary" className="w-1/4 px-8 py-10">
-                    <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
+                    <h1 className="font-semibold text-2xl border-b pb-8">Récapitulatif de commande</h1>
                     <div className="flex justify-between mt-10 mb-5">
                         <span className="font-semibold text-sm uppercase">Items 3</span>
                         <span className="font-semibold text-sm">590$</span>
                     </div>
                     <div>
-                        <label className="font-medium inline-block mb-3 text-sm uppercase">Shipping</label>
+                        <label className="font-medium inline-block mb-3 text-sm uppercase">Frais de ports</label>
                         <select className="block p-2 text-gray-600 w-full text-sm">
-                            <option>Standard shipping - $10.00</option>
+                            <option>Livraison standard - $10.00</option>
+                            <option>Livraison premium - $16.00</option>
                         </select>
                     </div>
                     <div className="py-10">
-                        <label htmlFor="promo" className="font-semibold inline-block mb-3 text-sm uppercase">Promo Code</label>
-                        <input type="text" id="promo" placeholder="Enter your code" className="p-2 text-sm w-full" />
+                        <label htmlFor="promo" className="font-semibold inline-block mb-3 text-sm uppercase">Code Promo</label>
+                        <input type="text" id="promo" placeholder="Entrer un code" className="p-2 text-sm w-full" />
                     </div>
-                    <button className="bg-red-500 hover-bg-red-600 px-5 py-2 text-sm text-white uppercase">Apply</button>
+                    <button className="bg-red-500 hover-bg-red-600 px-5 py-2 text-sm text-white uppercase">Appliquer</button>
                     <div className="border-t mt-8">
                         <div className="flex font-semibold justify-between py-6 text-sm uppercase">
-                            <span>Total cost</span>
+                            <span>Prix total</span>
                             <span>$600</span>
                         </div>
-                        <button className="bg-indigo-500 font-semibold hover-bg-indigo-600 py-3 text-sm text-white uppercase w-full">Checkout</button>
+                        <button className="bg-indigo-500 font-semibold hover-bg-indigo-600 py-3 text-sm text-white uppercase w-full">ACHETER</button>
                     </div>
                 </div>
             </div>
